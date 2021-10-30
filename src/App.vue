12952ddb4c5e190f01445c3ee4fe5a367e932d5b
xiekecheng <xkccoding@gmail.com>
2021-10-30 17:47:39 +0800

    处理页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Layout from './views/layout/index.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Layout/>
+
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
+html,body,#app{
+  height: 100%;
+}
+
 </style>
---
+++ src/views/layout/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>这里是Header标题</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	setup () {
+		
+
+		return {}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
