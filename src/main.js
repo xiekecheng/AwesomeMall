d380b6c6302825f59953864851e68a487353406f
xiekecheng <xkccoding@gmail.com>
2021-11-11 00:14:48 +0800

    ✨ feat: 引入vuex

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import 'element-plus/dist/index.css';
 import App from './App.vue';
 import 'normalize.css';
 import router from './router';
+import {store} from '@/store';
 const app = createApp(App);
 
 app.use(ElementPlus, {
   locale: zhCn,
 });
 app.use(router);
+app.use(store)
 app.mount('#app');
