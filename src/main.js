58c88c6d949fc1177563da19dc72c55e58d9911d
xiekecheng <xkccoding@gmail.com>
2021-11-14 23:36:36 +0800

    📃 docs: 添加注释

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import ElementPlus from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import 'element-plus/dist/index.css';
 import App from './App.vue';
+// 全局样式初始化
 import 'normalize.css';
+// 引入路由
 import router from './router';
+// 引入Store
 import {store} from '@/store';
 const app = createApp(App);
 
